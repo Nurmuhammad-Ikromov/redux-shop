fffaa6e599e9c449c9612240115e18f345fb0489
Nurmuhammad-Ikromov <nurmuhammadikromov58@gmail.com>
2025-02-11 10:22:02 +0500

    First commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = () => {
   const quantity = 5;
   return (
-    <div className="px-2 py-1 rounded border-black border rounded-full">
+    <div className="px-2 py-1 border-black border rounded-full">
       <span>Card: {quantity} Items</span>
     </div>
   );
